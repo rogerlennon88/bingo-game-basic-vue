ee5dacfe279491ce070cd33762b9f1a64e63d769
Roger Zea <rogerzea.dm@gmail.com>
2025-04-23 17:56:17 -0500

    Instalacion inicial del proyecto configurado con Vite.js en Vue.js

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './components/App.vue'
+import './style.css' // Si Vite creó un archivo de estilos global
+
+createApp(App).mount('#app')

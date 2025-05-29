37a57c6d39c1ff58e1847de48f14f9e9a3d80caa
Roger Zea <rogerzea.dm@gmail.com>
2025-05-29 09:02:54 -0500

    Mejoras visuales en los estilos de CounterView

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -33,37 +33,39 @@ onMounted(() => {
 <style scoped>
 #counter-view {
   background-color: rgb(169, 169, 169);
-  color: rgb(169, 169, 169);
+  color: rgb(211, 211, 211);
   font-size: 8rem;
+  line-height: 1;
+  text-shadow: 0 0 8px rgba(112, 128, 144, 0.8);
   padding: 8px;
   border-radius: 10px;
   display: grid;
   grid-auto-flow: column;
+  grid-template-columns: repeat(5, 1fr);
   gap: calc(var(--gap) / 2);
-  place-content: center;
 }
 #counter-view .data {
-  background-color: rgb(129, 133, 137);
-  padding: var(--gap);
+  padding: calc(var(--gap) / 2);
   display: grid;
   place-items: center;
 }
 #counter-view .data.counter-view-data-1,
-#counter-view .data.counter-view-data-2 {
+#counter-view .data.counter-view-data-2 { 
   font-weight: var(--fw-bold);
-  aspect-ratio: 4 / 3;
 }
 #counter-view .data.counter-view-data-1 {
   border-radius: 8px 0 0 8px;
+  grid-column: 2 span;
+  justify-content: end;
 }
 #counter-view .data.counter-view-div {
-  background-color: inherit;
-  color: rgb(129, 133, 137);
+  grid-column: 1 span;
 }
 #counter-view .data.counter-view-data-2 {
   border-radius: 0 8px 8px 0;
+  grid-column: 2 span;
 }
 #counter-view .data.actived {
-    color:  rgb(244, 187, 68);
+  color: var(--color-white);
 }
 </style>
